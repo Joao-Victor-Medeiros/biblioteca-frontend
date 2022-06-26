5d02e7b61011459c8b2f3df571278264a25e8671
Joao Victor Medeiros <joaovictormedeiros1505@gmail.com>
2022-06-26 12:52:21 -0300

    implement form and table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'biblioteca';
+  content:any = []
+
+  books = new FormGroup({
+    name: new FormControl(''),
+    author: new FormControl(''),
+    date: new FormControl('')
+  })
+
+  submit(){
+    console.log(this.books.value)
+  }
+
 }
